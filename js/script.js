8306a6bf03102e5123991bc0eddb0259e8cd1478
Pietroarmill <pietroarmillotta38@gmail.com>
2022-04-27 14:06:33 +0200

    add stringNumber inner HTML

--- js/script.js
+++ js/script.js
@@ -6,9 +6,10 @@
 
 // []al click di un pulsante vengono generati e visualizzati a schermo 5 numeri casuali
 const startBtn = document.getElementById("start");
+const timer = 5;
 
 startBtn.addEventListener("click", function() {
-
+    let numberString =  document.getElementById("rnd-numbers")
     const arrayRndNumbers = [];
     
     for (let i = 0; i < 5; i++) {
@@ -20,17 +21,23 @@ startBtn.addEventListener("click", function() {
     console.log(arrayRndNumbers);
 
     // []parte un countdown di 30 secondi 
+    let counter = document.getElementById("counter");
+    counter.innerHTML = timer;
 
-    let counter = 3;
-    let newYearCountdown = setInterval(function(){
-    console.log(counter);
-    counter--
-    if (counter <= 0) {
-        console.log("HAPPY NEW YEAR!!");
-        clearInterval(newYearCountdown);
-    }
+    let countdown = setInterval(function(){
+
+        counter.innerHTML--
+
+        if (counter.innerHTML <= 0) {
+            counter.innerHTML = "Tempo scaduto!!";
+            clearInterval(countdown);
+            numberString.innerHTML = "";
+        }   else {
+            numberString.innerHTML = arrayRndNumbers;
+        }
     }, 1000);
 
+    this.style.pointerEvents = "none"
 })
 
 
