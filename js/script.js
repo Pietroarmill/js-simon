14cefba6114019a1846698e35a456254c770ff7d
Pietroarmill <pietroarmillotta38@gmail.com>
2022-04-28 18:30:43 +0200

    add arrayNotGuessNumbers

--- js/script.js
+++ js/script.js
@@ -8,19 +8,19 @@
 const startBtn = document.getElementById("start");
 // const timer = 9995;
 
-startBtn.addEventListener("click", function() {
+startBtn.addEventListener("click", function () {
     startBtn.style.display = "none";
     let tryAgain = document.getElementById("try-again");
     let timer;
     let mumberOfNumbers;
     const userChoice = parseInt(document.getElementById("difficolta").value);
-    if( userChoice === 1 ) {
+    if (userChoice === 1) {
         timer = 31;
         mumberOfNumbers = 5;
-    }   else if ( userChoice === 2 ) {
+    } else if (userChoice === 2) {
         timer = 21;
         mumberOfNumbers = 6;
-    }   else {
+    } else {
         timer = 11;
         mumberOfNumbers = 7;
     }
@@ -28,15 +28,15 @@ startBtn.addEventListener("click", function() {
     let textString = document.getElementById("rnd-numbers");
     let numbersString = document.getElementById("rnd-numbers");
     // const arrayUserNumbers = [];
-    
+
     const arrayRndNumbers = [];
 
     do {
         const thisNumber = getIntNumbers(1, 100);
-        if ( !arrayRndNumbers.includes(thisNumber)) {
+        if (!arrayRndNumbers.includes(thisNumber)) {
             arrayRndNumbers.push(thisNumber);
         }
-    } while ( arrayRndNumbers.length != mumberOfNumbers );
+    } while (arrayRndNumbers.length != mumberOfNumbers);
     console.log(arrayRndNumbers);
     console.log(arrayRndNumbers.length, "sono i numeri generati");
 
@@ -45,38 +45,57 @@ startBtn.addEventListener("click", function() {
     // []e appaiono 5 prompt dove l'utente dovra inserire i numeri che ricorda di aver visto
     let countdown = document.getElementById("counter");
     let counter = timer;
-    let userCorrectNumbers = document.getElementById("user-numbers");
-    let numbersGuess = document.getElementById("pc-numbers");
+    let userCorrectNumbers = document.getElementById("user-correct-numbers");
+    let userIncorrectNumbers = document.getElementById("incorrect-numbers");
+    let numbersGuess = document.getElementById("numbers-guess");
 
-    let myInterval = setInterval(function(){
+    let myInterval = setInterval(function () {
 
         counter--
         // console.log(counter);
         if (counter <= -1) {
             const arrayCorrectNumbers = [];
+            const arrayIncorrectNumbers = [];
+            const arrayNotGuessNumbers =[];
+            const arrayUserNumbers = [];
+
 
             for (let i = 0; i < arrayRndNumbers.length; i++) {
 
+                const thisRndNumber = arrayRndNumbers[i];
                 const userNumber = parseInt(prompt("Dimmi un numero che hai visto."));
-                if ( arrayRndNumbers.includes(userNumber) && !arrayCorrectNumbers.includes(userNumber)) {
-                    arrayCorrectNumbers.push(userNumber)
+
+                arrayUserNumbers.push(userNumber);
+                console.log(arrayUserNumbers);
+                if (arrayRndNumbers.includes(userNumber) && !arrayCorrectNumbers.includes(userNumber)) {
+                    arrayCorrectNumbers.push(userNumber);
                     let userCorrectNumbers = document.getElementById("user-numbers");
+                } else {
+                    arrayIncorrectNumbers.push(userNumber);
                 }
+
+                // if (!arrayCorrectNumbers.includes(thisRndNumber)) {
+
+                //     arrayNotGuessNumbers.push(thisRndNumber);
+                //     console.log("i numeri non indovinati:",arrayNotGuessNumbers);
+
+                // }
                 console.log(userNumber);
-                console.log(arrayCorrectNumbers.length,"i numeri indovinati");
-                userCorrectNumbers.innerHTML = `Hai indovinato ${arrayCorrectNumbers.length} numeri: ${arrayCorrectNumbers}`
+                console.log(arrayCorrectNumbers.length, "i numeri indovinati");
+                userCorrectNumbers.innerHTML = `Hai indovinato ${arrayCorrectNumbers.length} numeri: <span class="green">${arrayCorrectNumbers},</span><span class="red">${arrayIncorrectNumbers}</span>`
                 countdown.innerHTML = ""
-                numbersGuess.innerHTML = `di ${arrayRndNumbers.length}: ${arrayRndNumbers}`;
+                numbersGuess.innerHTML = `su ${arrayRndNumbers.length}: ${arrayRndNumbers}`;
+                userIncorrectNumbers.innerHTML = `di cui errati ${arrayIncorrectNumbers.length}: `
             }
             clearInterval(myInterval);
 
-        }   else if ( counter <= 0 ) {
+        } else if (counter <= 0) {
             textString.innerHTML = "";
             numbersString.innerHTML = "";
             countdown.innerHTML = "Tempo scaduto!!";
             tryAgain.style.display = "block"
 
-        }   else {
+        } else {
             countdown.innerHTML = `Tempo a disposizione: ${counter} secondi.`;
             textString.innerHTML = `Memorizza questi numeri:`;
             numbersString.innerHTML = `${arrayRndNumbers}.`;
@@ -85,7 +104,7 @@ startBtn.addEventListener("click", function() {
 
     this.style.pointerEvents = "none";
 
-    tryAgain.addEventListener("click", function() {
+    tryAgain.addEventListener("click", function () {
 
         userCorrectNumbers.innerHTML = "";
         numbersGuess.innerHTML = "";
@@ -102,6 +121,6 @@ startBtn.addEventListener("click", function() {
 
 // Function
 function getIntNumbers(min, max) {
-    const number = Math.floor(Math.random() * max ) + min;
+    const number = Math.floor(Math.random() * max) + min;
     return number;
 }
