d5bee1a9fa1b45bf0f3c27e034f5507d0c073e20
Pietroarmill <pietroarmillotta38@gmail.com>
2022-04-28 13:52:59 +0200

    add tryAgain

--- js/script.js
+++ js/script.js
@@ -9,35 +9,44 @@ const startBtn = document.getElementById("start");
 // const timer = 9995;
 
 startBtn.addEventListener("click", function() {
+    startBtn.style.display = "none";
+    let tryAgain = document.getElementById("try-again");
     let timer;
+    let mumberOfNumbers;
     const userChoice = parseInt(document.getElementById("difficolta").value);
     if( userChoice === 1 ) {
         timer = 31;
+        mumberOfNumbers = 5;
     }   else if ( userChoice === 2 ) {
         timer = 21;
+        mumberOfNumbers = 6;
     }   else {
-        timer = 6;
+        timer = 11;
+        mumberOfNumbers = 7;
     }
 
-    let numberString =  document.getElementById("rnd-numbers");
+    let textString = document.getElementById("rnd-numbers");
+    let numbersString = document.getElementById("rnd-numbers");
     // const arrayUserNumbers = [];
     
     const arrayRndNumbers = [];
-    console.log(arrayRndNumbers);
 
     do {
         const thisNumber = getIntNumbers(1, 100);
         if ( !arrayRndNumbers.includes(thisNumber)) {
             arrayRndNumbers.push(thisNumber);
         }
-    } while ( arrayRndNumbers.length != 5 );
-    console.log(arrayRndNumbers.length);
+    } while ( arrayRndNumbers.length != mumberOfNumbers );
+    console.log(arrayRndNumbers);
+    console.log(arrayRndNumbers.length, "sono i numeri generati");
 
     // []parte un countdown di 30 secondi 
     // []al termine del countdown i numeri spariscono 
     // []e appaiono 5 prompt dove l'utente dovra inserire i numeri che ricorda di aver visto
     let countdown = document.getElementById("counter");
     let counter = timer;
+    let userCorrectNumbers = document.getElementById("user-numbers");
+    let numbersGuess = document.getElementById("pc-numbers");
 
     let myInterval = setInterval(function(){
 
@@ -49,30 +58,42 @@ startBtn.addEventListener("click", function() {
             for (let i = 0; i < arrayRndNumbers.length; i++) {
 
                 const userNumber = parseInt(prompt("Dimmi un numero che hai visto."));
-                let userCorrectNumbers = document.getElementById("user-numbers")
                 if ( arrayRndNumbers.includes(userNumber) && !arrayCorrectNumbers.includes(userNumber)) {
                     arrayCorrectNumbers.push(userNumber)
                     let userCorrectNumbers = document.getElementById("user-numbers");
                 }
                 console.log(userNumber);
                 console.log(arrayCorrectNumbers.length,"i numeri indovinati");
-                console.log(":",arrayCorrectNumbers);
-                userCorrectNumbers.innerHTML = `Hai indovinato ${arrayCorrectNumbers.length} numeri: ${arrayCorrectNumbers}.`
+                userCorrectNumbers.innerHTML = `Hai indovinato ${arrayCorrectNumbers.length} numeri: ${arrayCorrectNumbers}`
                 countdown.innerHTML = ""
+                numbersGuess.innerHTML = `di ${arrayRndNumbers.length}: ${arrayRndNumbers}`;
             }
             clearInterval(myInterval);
 
         }   else if ( counter <= 0 ) {
-            numberString.innerHTML = "";
+            textString.innerHTML = "";
+            numbersString.innerHTML = "";
             countdown.innerHTML = "Tempo scaduto!!";
+            tryAgain.style.display = "block"
 
         }   else {
-            countdown.innerHTML = `tempo a disposizione: ${counter} secondi.`;
-            numberString.innerHTML = `memorizza questi numeri: ${arrayRndNumbers}.`;
+            countdown.innerHTML = `Tempo a disposizione: ${counter} secondi.`;
+            textString.innerHTML = `Memorizza questi numeri:`;
+            numbersString.innerHTML = `${arrayRndNumbers}.`;
         }
     }, 1000);
 
-    this.style.pointerEvents = "none"
+    this.style.pointerEvents = "none";
+
+    tryAgain.addEventListener("click", function() {
+
+        userCorrectNumbers.innerHTML = "";
+        numbersGuess.innerHTML = "";
+        tryAgain.style.display = "none"
+        startBtn.style.pointerEvents = "visible";
+        startBtn.style.display = "block";
+
+    })
 
 })
 
